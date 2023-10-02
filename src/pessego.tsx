dd70836f6398420489555af570778eef27898274
Brayan Santos <brayan.santosrosa@gmail.com>
2023-10-02 09:07:04 -0300

    Creating Front-end of Project Personal Blog

---
+++ src/pessego.tsx
@@ -0,0 +1,11 @@
+function Pessego() {
+
+return (
+  <>
+  <img src="https://www.infoescola.com/wp-content/uploads/2023/02/pessego.jpg" alt="Photo of Pessego"/>
+  </>  
+)
+}
+
+
+export default Pessego;
